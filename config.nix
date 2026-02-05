4d7a174f3334108e91eea270d3124bd5d74be71e
neonic-acid <othineon@gmail.com>
2026-02-05 13:20:26 +0300

    replace vim with emacs

--- config.nix
+++ config.nix
@@ -96,7 +96,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
-    vim
+    emacs
     wget
     qutebrowser
     umu-launcher
